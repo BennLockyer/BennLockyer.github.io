5e5557003018d50986e66001db24c49fc0bf6b7b
BennLockyer <benn.lockyer@hotmail.com>
2024-09-11 16:47:56 +1000

    Update script.js
    Removed x panning, locked garden width to canvas width. More test items

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ const canvas = document.getElementById('gardenCanvas');
 const canvasContext = canvas.getContext('2d');
 
 let isPanning = false;
-let startX, startY;
-let offsetX = 0, offsetY = 0;
+let startY;
+let offsetY = 0;
 
-const gardenWidth = 2000;
-const gardenHeight = 1500;
+const gardenWidth = canvas.width;
+const gardenHeight = 2500;
 
 const items = [
     { id: "item1", x: 50, y: 50, width: 100, height: 100, color: 'blue' },
     { id: "item2", x: 200, y: 100, width: 150, height: 100, color: 'green' }
+    { id: "item3", x: 150, y: 900, width: 80, height: 110, color: 'red' }
 ];
 
 function DrawItems() {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     canvasContext.save()
-    canvasContext.translate(offsetX, offsetY)
+    canvasContext.translate(0, offsetY)
     items.forEach(item => {
         canvasContext.fillStyle = item.color;
         canvasContext.fillRect(item.x, item.y, item.width, item.height);
@@ -28,16 +29,12 @@ function DrawItems() {
 
 canvas.addEventListener('mousedown', function (e) {
     isPanning = true;
-    startX = e.clientX - offsetX;
     startY = e.clientY - offsetY;
 });
 
 canvas.addEventListener('mousemove', function (e) {
     if (isPanning) {
-        const newOffsetX = e.clientX - startX;
         const newOffsetY = e.clientY - startY;
-
-        offsetX = Math.min(0, Math.max(canvas.width - gardenWidth, newOffsetX));
         offsetY = Math.min(0, Math.max(canvas.height - gardenHeight, newOffsetY));
 
         DrawItems();
